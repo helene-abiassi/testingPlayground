e628b4f6cd4470e5573178a1d552af9f8413f9fa
helene-abiassi <abiassi.helene@gmail.com>
2024-07-16 12:48:06 +0300

    more tests

--- src/math.test.js
+++ src/math.test.js
@@ -3,7 +3,7 @@ import { add } from "./math";
 
 it("should summarize all numbers in an array", () => {
   //Arrange
-  const numbers = [1, 2, 3];
+  const numbers = [1, 2, 3, 5, 6, 7, 89];
 
   //Act
   const result = add(numbers);
@@ -14,4 +14,41 @@ it("should summarize all numbers in an array", () => {
     0
   );
   expect(result).toBe(expectedResult);
+});
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  const invalidInputs = ["NaN", 1];
+
+  const invalidResult = add(invalidInputs);
+
+  expect(invalidResult).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const stringNumbers = ["1", "2"];
+
+  const result = add(stringNumbers);
+
+  const expectedResult = stringNumbers.reduce(
+    (prevValue, curValue) => Number(prevValue) + Number(curValue),
+    0
+  );
+
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no numbers are passed to the array", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrowError();
 });
