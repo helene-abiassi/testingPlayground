cb08ca5df7aaea29f035391bf95bca748bd43a31
helene-abiassi <abiassi.helene@gmail.com>
2024-07-17 12:50:44 +0300

    working on my night cheese

--- src/utils/numbers.test.js
+++ src/utils/numbers.test.js
@@ -1,28 +1,34 @@
-import { expect, it } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, expect, it } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("should return a number when passed a string numerical input value", () => {
-  //Arrange
-  const inputValue = "2";
+describe("transformToNumbers()", () => {
+  it("should return a number when passed a string numerical input value", () => {
+    //Arrange
+    const inputValue = "2";
 
-  //Act
-  const numericValue = transformToNumber(inputValue);
+    //Act
+    const numericValue = transformToNumber(inputValue);
 
-  //Assert
-  expect(numericValue).toBeTypeOf("number");
-  //this is better than using toBeNaN, because NaN is of type Number
+    //Assert
+    expect(numericValue).toBeTypeOf("number");
+    //this is better than using toBeNaN, because NaN is of type Number
+  });
+
+  it("should throw an error if a non-numerical value is passed", () => {
+    const nonNumericValue = "Hello World";
+
+    const nonNumericResult = transformToNumber(nonNumericValue);
+
+    expect(nonNumericResult).toBeNaN();
+  });
 });
 
-it("should throw an error if a non-numerical value is passed", () => {
-  const nonNumericValue = "Hello World";
+describe("cleanNumbers()", () => {
+  it("should return numbers when passed numerical string values", () => {
+    const numbers = ["1", "2", "3"];
 
-  const nonNumericResult = transformToNumber(nonNumericValue);
+    const cleanedNumbers = cleanNumbers(numbers);
 
-  expect(nonNumericResult).toBeNaN();
-
-  //   const resultFn = () => {
-  //     transformToNumber(nonNumericValue);
-  //   };
-
-  //   expect(resultFn).toThrowError;
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
 });
