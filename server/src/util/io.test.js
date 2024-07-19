832139d0d123cb2ee9e63c1a04e728e1ad028cab
helene-abiassi <abiassi.helene@gmail.com>
2024-07-19 11:30:16 +0300

    learning about mocks

--- server/src/util/io.test.js
+++ server/src/util/io.test.js
@@ -2,8 +2,18 @@ import { it, expect, vi } from "vitest";
 import { promises as fs } from "fs";
 
 import writeData from "./io";
+import { join } from "path";
 
 vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it("should execute the writeFile method", () => {
   const testData = "Test";
@@ -11,5 +21,6 @@ it("should execute the writeFile method", () => {
 
   writeData(testData, testFilename);
 
-  expect(fs.writeFile).toBeCalled();
+  // expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
